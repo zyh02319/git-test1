a7902a6427c3706a5475dd336a35a8d209e096bb
zyh02319 <2365174401@qq.com>
2025-03-21 13:21:36 +0800

    second commit

--- src/main/java/org/example/gitTest.java
+++ src/main/java/org/example/gitTest.java
@@ -3,5 +3,6 @@ package org.example;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
